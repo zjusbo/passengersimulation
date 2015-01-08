3882a7a7f3ab39a1d5d4ff0628ae0133ed1a9f76
songbo <sbo@zju.edu.cn>
2015-01-08 22:30:12 +0800

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+class Person(object):
+	"""Person"""
+	def __init__(self, x, y, direction):
+		super(Person, self).__init__()
+		self.x = x
+		self.y = y
+		self.direction = direction
+	def move(self):
+		
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
